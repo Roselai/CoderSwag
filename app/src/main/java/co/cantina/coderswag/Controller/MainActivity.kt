e2d161e130a71199249dc8c4bbbaa96072de3023
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-27 13:24:27 -0400

    removed action bar

--- app/src/main/java/co/cantina/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/co/cantina/coderswag/Controller/MainActivity.kt
@@ -2,9 +2,7 @@ package co.cantina.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import co.cantina.coderswag.Adapters.CategoryAdapter
-import co.cantina.coderswag.Model.Category
 import co.cantina.coderswag.R
 import co.cantina.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
