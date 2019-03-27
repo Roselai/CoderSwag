32d2d08e0f6d1bcb7deb0a9ab315f733e686f3fa
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-27 12:29:21 -0400

    added list view adapter

--- app/src/main/java/co/cantina/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/co/cantina/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,21 @@ package co.cantina.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import co.cantina.coderswag.Model.Category
 import co.cantina.coderswag.R
+import co.cantina.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
