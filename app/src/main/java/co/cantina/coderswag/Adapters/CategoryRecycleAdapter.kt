8e047cea24bd6b3287ec48282be9ae87820acfe0
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-27 15:56:34 -0400

    fully implemented recycler view

--- app/src/main/java/co/cantina/coderswag/Adapters/CategoryRecycleAdapter.kt
+++ app/src/main/java/co/cantina/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -1,24 +1,41 @@
 package co.cantina.coderswag.Adapters
 
+import android.content.Context
+import android.support.v7.view.menu.MenuView
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import co.cantina.coderswag.Model.Category
+import co.cantina.coderswag.R
 
-class CategoryRecycleAdapter: RecyclerView.Adapter<CategoryRecycleAdapter.holder>() {
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category> ): RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): holder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.category_list_item, parent, false)
+
+        return Holder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return categories.count()
     }
 
-    override fun onBindViewHolder(p0: holder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindCategory(categories[position], context)
     }
 
-    inner class holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val categoryImage = itemView.findViewById<ImageView>(R.id.categoryImage)
+        val categoryName = itemView.findViewById<TextView>(R.id.categoryName)
 
+        fun bindCategory(category: Category, context: Context ){
+            val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName )
+            categoryImage.setImageResource(resourceId)
+            categoryName.text = category.title
+        }
     }
 }
--- app/src/main/java/co/cantina/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/co/cantina/coderswag/Controller/MainActivity.kt
@@ -2,22 +2,30 @@ package co.cantina.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
 import co.cantina.coderswag.Adapters.CategoryAdapter
+import co.cantina.coderswag.Adapters.CategoryRecycleAdapter
 import co.cantina.coderswag.R
 import co.cantina.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+
+        //optimization for when cells dont change size
+        categoryListView.setHasFixedSize(true)
 
 
 //        //doesn't work for recycler view
