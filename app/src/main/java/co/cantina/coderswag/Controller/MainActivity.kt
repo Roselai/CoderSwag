4a848615256a8a8db25036d0bb096453289dca7e
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-27 13:17:41 -0400

    replaced array adapter with category adapter

--- app/src/main/java/co/cantina/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/co/cantina/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package co.cantina.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import co.cantina.coderswag.Adapters.CategoryAdapter
 import co.cantina.coderswag.Model.Category
 import co.cantina.coderswag.R
 import co.cantina.coderswag.Services.DataService
@@ -10,13 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
