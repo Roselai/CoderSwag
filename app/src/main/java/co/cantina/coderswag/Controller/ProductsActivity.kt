85910b29d8202151a56764e850b3bd2516fb4c31
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-28 09:23:35 -0400

    updated products activity w/ products adapter

--- app/src/main/java/co/cantina/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/co/cantina/coderswag/Controller/ProductsActivity.kt
@@ -2,16 +2,27 @@ package co.cantina.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import co.cantina.coderswag.Adapters.ProductsAdapter
 import co.cantina.coderswag.R
+import co.cantina.coderswag.Services.DataService
 import co.cantina.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        val layoutManager = GridLayoutManager(this, 2)
+        productListView.layoutManager = layoutManager as RecyclerView.LayoutManager?
+        productListView.adapter = adapter
     }
 }
