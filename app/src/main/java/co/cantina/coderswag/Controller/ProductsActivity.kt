48474a3b55de8a86e3d9a5b2f68f0cc0e846b177
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-28 09:51:47 -0400

    changed num of columns for landscape view

--- app/src/main/java/co/cantina/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/co/cantina/coderswag/Controller/ProductsActivity.kt
@@ -1,5 +1,6 @@
 package co.cantina.coderswag.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
@@ -21,7 +22,14 @@ class ProductsActivity : AppCompatActivity() {
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
         adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
 
-        val layoutManager = GridLayoutManager(this, 2)
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
         productListView.layoutManager = layoutManager as RecyclerView.LayoutManager?
         productListView.adapter = adapter
     }
