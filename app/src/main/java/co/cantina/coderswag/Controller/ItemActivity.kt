be6f830258c61123dc291855069992d272b9f3b6
Shukti Shaikh <shukti.azad@gmail.com>
2019-03-28 13:02:56 -0400

    successfully showing items

--- app/src/main/java/co/cantina/coderswag/Controller/ItemActivity.kt
+++ app/src/main/java/co/cantina/coderswag/Controller/ItemActivity.kt
@@ -1,10 +1,13 @@
 package co.cantina.coderswag.Controller
 
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import co.cantina.coderswag.Model.Product
 import co.cantina.coderswag.R
 import co.cantina.coderswag.Utilities.EXTRA_PRODUCT
+import kotlinx.android.synthetic.main.activity_item.*
+import kotlinx.android.synthetic.main.product_list_item.*
 
 class ItemActivity : AppCompatActivity() {
 
@@ -16,5 +19,10 @@ class ItemActivity : AppCompatActivity() {
 
         product = intent.getParcelableExtra(EXTRA_PRODUCT)
         println(product.title)
+
+
+        val resourceId = this.resources.getIdentifier(product.image, "drawable", this.packageName )
+        itemImage.setImageResource(resourceId)
+        itemTitle.text = product.title
     }
 }
